9d7bb81573f29e68ad980c87c9d0178a5bf4ece5
EC2 Default User <ec2-user@ip-172-31-36-94.us-east-2.compute.internal>
2019-04-09 16:40:49 +0000

    Single Page Applications Part 1

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -5,7 +5,12 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
+import Contact from './ContactComponent';
 import { DISHES } from './dishes';
+import { COMMENTS } from './comments';
+import { PROMOTIONS } from './promotions';
+import { LEADERS } from './leaders';
+
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -14,6 +19,9 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
     };
   }
 
@@ -21,7 +29,11 @@ class Main extends Component {
     
     const HomePage = () => {
       return(
-          <Home />
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
       );
     }
     
@@ -31,6 +43,7 @@ class Main extends Component {
           <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />} />
               <Redirect to="/home" />
           </Switch>
         <Footer />
