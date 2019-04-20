987acbbfbe92b5d9e6787d3c3be0697330135f67
EC2 Default User <ec2-user@ip-172-31-36-94.us-east-2.compute.internal>
2019-04-20 17:15:12 +0000

    Assignment 2

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { DISHES } from './dishes';
 import { COMMENTS } from './comments';
 import { PROMOTIONS } from './promotions';
@@ -49,6 +49,7 @@ class Main extends Component {
         <Header />
           <Switch>
               <Route path='/home' component={HomePage} />
+              <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />} />
